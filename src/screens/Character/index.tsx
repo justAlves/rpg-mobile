836019898c3f880173c2b4eb418112139d2616a0
justAlves <iamalves.jpg@gmail.com>
2023-06-16 17:08:18 -0300

    feat: :sparkles: Character screen initialized

--- src/screens/Character/index.tsx
+++ src/screens/Character/index.tsx
@@ -1,6 +1,9 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, Modal } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { useRoute } from '@react-navigation/native'
+import api from '../../api'
+import { Container, SubTitle, Title } from '../Login/styles'
+import { Slider } from '@miblanchard/react-native-slider'
 
 export default function Character() {
 
@@ -8,9 +11,57 @@ export default function Character() {
 
   const { id } = route.params
 
+  const [character, setCharacter] = useState([])
+  const [dice, setDice] = useState(0 as number)
+  const [modal, setModal] = useState(false)
+
+  useEffect(() => {
+    async function getCharacter() {
+      try {
+        const response = await api.get(`/character?id=${id}`)
+
+        setCharacter(response.data.character)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    getCharacter()
+  }, [])
+
+  function rollDice( faces: number ) {
+    const dice = Math.floor(Math.random() * faces) + 1
+
+    setDice(dice)
+    setModal(true)
+  }
+
   return (
-    <View>
-      <Text>{id}</Text>
-    </View>
+    <Container>
+      <Title>{character.name}</Title>
+      <Slider
+        value={character.currentLife}
+        minimumValue={0}
+        maximumValue={character.maxLife}
+        trackStyle={{ width: "100%", height: 10, backgroundColor: "#7c5b5b" }}
+        thumbStyle={{ width: 0, height: 0 }}
+        minimumTrackTintColor="#df3d3d"
+        containerStyle={{ height: 20, width: "80%" }}
+        disabled
+      />
+      <Slider
+        value={character.currentSanity}
+        minimumValue={0}
+        maximumValue={character.maxSanity}
+        trackStyle={{ width: "100%", height: 10, backgroundColor: "#5d5b7c" }}
+        thumbStyle={{ width: 0, height: 0 }}
+        minimumTrackTintColor="#3d4ddf"
+        containerStyle={{ height: 20, width: "80%" }}
+        disabled
+      />
+      <TouchableOpacity onPress={() => rollDice(character.strength)}>
+        <Text>Roll</Text>
+      </TouchableOpacity>
+    </Container>
   )
 }
